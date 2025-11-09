3d7b547bd76338980ec749aeb98c2d989a7e8fd3
juan david <dabson753@gmail.com>
2025-11-09 15:35:37 -0600

    Se agregaron los primeros paquetes

---
+++ POOEjercicios/src/main/java/com/mycompany/pooejercicios/POOEjercicios.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pooejercicios;
+
+/**
+ *
+ * @author tadeo
+ */
+public class POOEjercicios {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
